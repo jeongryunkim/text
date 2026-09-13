7e90d59dc9ebb4ede0b729cb28a37586e4889c14
jeongryunkim <wingsword1@gmail.com>
2026-09-13 20:49:53 +0900

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-with open("C:/Users/KDA/OneDrive/Python/Scripts/260910/Lotto & Updown.txt", "r", encoding="utf-8") as file:
-	lines = file.readlines()
-
-# print(lines)
-
-for line in lines:
-	print(line.strip())
